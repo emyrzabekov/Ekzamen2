bbc5103e4a27c3f3ab22ec3c181d609cd684ae7e
Your Name <you@example.com>
2021-05-15 21:20:43 +0600

    initial commit

---
+++ task2.py
@@ -0,0 +1,21 @@
+my_file = open("../exam/alfa.txt", "r")
+file_contents = my_file.read()
+b = file_contents.split()
+b = ''.join(b)
+b = b.replace('.', '').replace(',', '').replace('!', '').replace('\n', '').replace('?', '').replace('-', '').replace('‘', '').replace('’', '')
+new_letter = b.lower()
+start = -1
+count = 0
+while True:
+    start = new_letter.find("s", start+1)
+    if start == -1:
+        break
+    count += 1
+print("Букв s: ", count)
+
+while True:
+    start = new_letter.find("t", start+1)
+    if start == -1:
+        break
+    count += 1
+print('Букв t: ', count)
---
+++ task3.py
@@ -0,0 +1,72 @@
+class Course:
+
+    def __init__(self, spisok):
+
+        self.spisok = spisok
+
+    def vyvod(self):
+        courses = {'python':[],
+                   'java':[],
+                   'javascript':[]}
+
+        for k in self.spisok:
+            # print(k['name'])
+            if k['course'] == 'python':
+                courses['python'].append(k['name'])
+            elif k['course'] == 'java':
+                courses['java'].append(k['name'])
+            elif k['course'] == 'javascript':
+                courses['javascript'].append(k['name'])
+        print(courses)
+
+    def name_age(self):
+        names1 = {}
+
+        for z in self.spisok:
+            names = {z['name']:z['age']}
+            names1.update(names)
+        print(names1)
+
+
+
+
+ucheba = Course([
+    {'name': 'John', 'age': 15, 'course': 'python', 'gender': 'Male'},
+    {'name': 'Andrew', 'age': 20, 'course': 'javascript', 'gender': 'Male'},
+    {'name': 'Marcus', 'age': 25, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Agness', 'age': 40, 'course': 'python', 'gender': 'Female'},
+    {'name': 'Don', 'age': 42, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Michael', 'age': 17, 'course': 'javascript', 'gender': 'Male'},
+    {'name': 'Jennifer', 'age': 16, 'course': 'java', 'gender': 'Female'},
+    {'name': 'Angela', 'age': 16, 'course': 'python', 'gender': 'Female'},
+    {'name': 'Eniston', 'age': 34, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Albert', 'age': 33, 'course': 'javascript', 'gender': 'Male'},
+    {'name': 'Nash', 'age': 28, 'course': 'python', 'gender': 'Male'},
+    {'name': 'Nicolas', 'age': 19, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Alexey', 'age': 21, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Martin', 'age': 22, 'course': 'python', 'gender': 'Male'},
+    {'name': 'Gloria', 'age': 23, 'course': 'java', 'gender': 'Female'},
+    {'name': 'Mikel', 'age': 24, 'course': 'python', 'gender': 'Male'},
+    {'name': 'Susanne', 'age': 25, 'course': 'javascript', 'gender': 'Female'},
+    {'name': 'Stevie', 'age': 26, 'course': 'python', 'gender': 'Male'},
+    {'name': 'Mark', 'age': 18, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Johnathan', 'age': 15, 'course': 'python', 'gender': 'Male'},
+    {'name': 'Aidin', 'age': 20, 'course': 'javascript', 'gender': 'Male'},
+    {'name': 'Mirbek', 'age': 25, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Aidana', 'age': 40, 'course': 'python', 'gender': 'Female'},
+    {'name': 'Atay', 'age': 42, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Chyngyz', 'age': 17, 'course': 'javascript', 'gender': 'Male'},
+    {'name': 'Aigerim', 'age': 16, 'course': 'java', 'gender': 'Female'},
+    {'name': 'Jyldyz', 'age': 16, 'course': 'python', 'gender': 'Female'},
+    {'name': 'Emir', 'age': 34, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Emirlan', 'age': 12, 'course': 'javascript', 'gender': 'Male'},
+    {'name': 'Nursultan', 'age': 13, 'course': 'python', 'gender': 'Male'},
+    {'name': 'Aliaskar', 'age': 19, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Aktanbek', 'age': 21, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Adyl', 'age': 14, 'course': 'python', 'gender': 'Male'},
+    {'name': 'Janyl', 'age': 16, 'course': 'python', 'gender': 'Female'},
+    {'name': 'Meerim', 'age': 12, 'course': 'javascript', 'gender': 'Female'},
+    {'name': 'Sultan', 'age': 13, 'course': 'python', 'gender': 'Male'},
+])
+ucheba.name_age()
+ucheba.vyvod()
